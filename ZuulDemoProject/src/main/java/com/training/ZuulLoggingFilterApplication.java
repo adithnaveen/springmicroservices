c79549c71df43a02aa8d5fddb72942b13c5ef9f2
Naveen <adith.naveen@gmail.com>
2018-08-28 01:07:06 +0530

    added zuul project

--- ZuulDemoProject/src/main/java/com/training/ZuulLoggingFilterApplication.java
+++ ZuulDemoProject/src/main/java/com/training/ZuulLoggingFilterApplication.java
@@ -2,10 +2,10 @@ package com.training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 //@EnableDiscoveryClient
 @EnableZuulProxy
@@ -16,5 +16,20 @@ public class ZuulLoggingFilterApplication {
 		SpringApplication.run(ZuulLoggingFilterApplication.class, args);
 	}
 	
-	
+	@Bean
+	public PreFilter preFilter() {
+	    return new PreFilter();
+	}
+	@Bean
+	public PostFilter postFilter() {
+	    return new PostFilter();
+	}
+	@Bean
+	public ErrorFilter errorFilter() {
+	    return new ErrorFilter();
+	}
+	@Bean
+	public RouteFilter routeFilter() {
+	    return new RouteFilter();
+	}
 }
